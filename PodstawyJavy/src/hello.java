215f7d47ad30cae209f91d08a31e127e619bde4b
Tomek4k <tomaszwcislo10@gmail.com>
2025-09-10 12:25:08 +0200

    Pierwszy Program

--- PodstawyJavy/src/hello.java
+++ PodstawyJavy/src/hello.java
@@ -1,5 +1,18 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("hello word");
+        //kontrolowanie wielu lini
+        /*
+        fdsfdsdsf
+        sd
+
+        sd
+        s
+
+
+        sd
+         */
+        int a = 5, b = 2;
+        System.out.println(a+b);
     }
 }
